8dadac970b8e8e3de21688ed26ed8571a08b77ea
Anjas Wicaksana <32026261+anjaswicak@users.noreply.github.com>
2025-07-30 14:37:53 +0700

    update final

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,7 +1,7 @@
 export default function ApplicationLogo(props) {
     return (
         <img
-            src="/storage/assets/img/logo-new.png"
+            src="/assets/logo-new.png"
             alt="Logo"
             {...props}
             style={{ height: 70, width: 192, backgroundColor: '#232323', ...props.style }}
